936c1a1536184f2dae08f07e1ee66b72d46406c7
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-27 13:05:26 -0300

    fetch posts from api and mount list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import api from "../services/api";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  posts: Observable<any>;
 
   constructor() {}
 
+  ngOnInit() {
+    api.get("/posts.json").then((response) => {
+      this.posts = response.data.map((post) => ({
+        ...post,
+        thumbnail: `http://localhost:3000${post.thumbnail.url}`,
+      }));
+      console.log(this.posts);
+    });
+  }
 }
